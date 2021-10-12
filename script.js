84301dfa74e41887b20eb147fc509a657b08715d
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-12 15:14:12 +0100

    Done Ex 15, 18, 20

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ changeMainTItle()
 
 const changeJumbotronBackground = () => {
     const jumbotron = document.querySelector('.jumbotron')
-    console.log(jumbotron)
     jumbotron.classList.remove('bg-dark')
     jumbotron.classList.add('bg-primary')
 }
@@ -32,13 +31,23 @@ changeJumbotronBackground()
 
 // EX14) Write a function to remove all the links under "Elsewhere"
 
+// TODO: 
 
+const removeLinksUnderElsewhere = () => {
 
+}
+
+removeLinksUnderElsewhere()
 
 // EX15) Write a function to change the column size for heading in jumbotron
 
+changeJumbotronHeadingColumnSize = () => {
+    const jumbotronHeadingContainer = document.querySelector('.jumbotron div')
+    jumbotronHeadingContainer.classList.remove('col-md-6')
+    jumbotronHeadingContainer.classList.add('col-12')
+}
 
-
+changeJumbotronHeadingColumnSize()
 
 // EX16) Write a function to remove the "Search" magnifying glass icon
 
@@ -52,8 +61,24 @@ changeJumbotronBackground()
 
 // EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
 
+const addNewBlogPost = () => {
+    const addBtn = document.querySelector('.blog-pagination a:last-child')
+    addBtn.addEventListener('click', createNewBlogPost)
+}
 
+const createNewBlogPost = () => {
+    const blogContainer = document.createElement('div')
+    blogContainer.classList.add('blog-post')
+    const blogTitle = document.createElement('h2')
+    blogTitle.classList.add('blog-post-title')
+    blogTitle.innerText = 'New Blog Post'
+    blogContainer.appendChild(blogTitle)
+    const blogButtons = document.querySelector('.blog-pagination')
+    const allBlogPostsContainer = document.querySelector('.blog-main')
+    allBlogPostsContainer.insertBefore(blogContainer, blogButtons)
+}
 
+addNewBlogPost()
 
 // EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
 
@@ -62,6 +87,15 @@ changeJumbotronBackground()
 
 // EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
 
+const alertAuthorWhenHover = () => {
+    const author = document.querySelectorAll('.blog-post-meta a')
+    author.forEach(person => {
+        person.addEventListener('mouseover', () => {
+            const authorName = person.innerText
+            alert(authorName)
+        })
+    })
+}
 
-
+alertAuthorWhenHover()
 
